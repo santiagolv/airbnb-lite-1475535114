5d4dea5bf9bc0788fc5d550301cfd1ebbfff832d
Raghu Betina <raghu@firstdraft.com>
2016-10-03 22:54:50 +0000

    Direct association between users and messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,8 @@
 class Message < ApplicationRecord
   # Direct associations
 
+  belongs_to :user
+
   belongs_to :listing
 
   # Indirect associations
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :messages,
+             :dependent => :destroy
+
   has_many   :user_reviews,
              :dependent => :destroy
 
