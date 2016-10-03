bb2bab3c1f0914d79584b503566296fb1e2acecc
Raghu Betina <raghu@firstdraft.com>
2016-10-03 22:54:50 +0000

    Direct association between listings and policies

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,9 @@
 class Listing < ApplicationRecord
   # Direct associations
 
+  has_many   :policies,
+             :dependent => :destroy
+
   has_many   :listing_photos,
              :dependent => :destroy
 
--- app/models/policy.rb
+++ app/models/policy.rb
@@ -1,6 +1,8 @@
 class Policy < ApplicationRecord
   # Direct associations
 
+  belongs_to :listing
+
   # Indirect associations
 
   # Validations
