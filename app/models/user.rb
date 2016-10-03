1221875d1c5169ab28619c9e4addf795a34a8cf0
Raghu Betina <raghu@firstdraft.com>
2016-10-03 22:54:50 +0000

    Direct association between users and user_reviews

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :user_reviews,
+             :dependent => :destroy
+
   has_many   :reviews,
              :class_name => "ListingReview",
              :dependent => :destroy
