0e7bb32367e34b7086fbb34b3c6e84303ab9639d
Raghu Betina <raghu@firstdraft.com>
2016-10-03 22:54:50 +0000

    Direct association between listings and ownerships

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,9 @@
 class Listing < ApplicationRecord
   # Direct associations
 
+  has_one    :ownership,
+             :dependent => :destroy
+
   has_many   :available_days,
              :dependent => :destroy
 
