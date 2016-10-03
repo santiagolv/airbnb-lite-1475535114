b7d4677712a86f7bb6550b2dfbd7050aceeaecd0
Raghu Betina <raghu@firstdraft.com>
2016-10-03 22:55:59 +0000

    Whitelist columns in ListingReview for ActiveAdmin

--- app/admin/listing_review.rb
+++ app/admin/listing_review.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register ListingReview do
 
+ permit_params :review_body, :accuracy, :communications, :cleanliness, :location, :check_in, :value, :user_id, :listing_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
