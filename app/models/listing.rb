6420d0254dbaf5581e0440312d0a0067ab3cc84a
Raghu Betina <raghu@firstdraft.com>
2016-10-03 22:54:50 +0000

    Direct association between listings and bookmarks

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,9 @@
 class Listing < ApplicationRecord
   # Direct associations
 
+  has_many   :bookmarks,
+             :dependent => :destroy
+
   has_one    :ownership,
              :dependent => :destroy
 
